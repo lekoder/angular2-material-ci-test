a9a665253ed1cd96b22f85a4fac0a7a2e745067c
Mariusz 'koder' Chwalba <mariusz.chwalba@cashbill.pl>
2016-12-21 09:18:03 +0100

    Working tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,17 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MaterialModule.forRoot()
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     });
     TestBed.compileComponents();
